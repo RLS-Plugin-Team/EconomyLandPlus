bf50da912ec035e782f37a99906870c7f6b147be
narapon <43005911+narapons@users.noreply.github.com>
2019-01-10 14:55:14 +0900

    EconomyLandPlus

--- src/onebone/economyland/event/LandRemoveEvent.php
+++ src/onebone/economyland/event/LandRemoveEvent.php
@@ -20,58 +20,19 @@
 
 namespace onebone\economyland\event;
 
+use pocketmine\event\Cancellable;
 use pocketmine\event\Event;
 
-class LandAddedEvent extends  Event{
+class LandRemoveEvent extends Event implements Cancellable{
 	public static $handlerList = null;
 
-	private $id, $startX, $startZ, $endX, $endZ, $level, $price, $player, $expires;
+	private $id;
 
-	public function __construct($id, $startX, $startZ, $endX, $endZ, $level, $price, $player, $expires){
-		$this->startX = $startX;
-		$this->startZ = $startZ;
-		$this->endX = $endX;
-		$this->endZ = $endZ;
-		$this->level = $level;
-		$this->price = $price;
+	public function __construct($id){
 		$this->id = $id;
-		$this->player = $player;
-		$this->expires = $expires;
 	}
 
 	public function getId(){
 		return $this->id;
-	}
-
-	public function getStartX(){
-		return $this->startX;
-	}
-
-	public function getStartZ(){
-		return $this->startZ;
-	}
-
-	public function getEndX(){
-		return $this->endX;
-	}
-
-	public function getEndZ(){
-		return $this->endZ;
-	}
-
-	public function getLevel(){
-		return $this->level;
-	}
-
-	public function getPrice(){
-		return $this->price;
-	}
-
-	public function getExpires(){
-		return $this->expires;
-	}
-
-	public function getPlayer(){
-		return $this->player;
 	}
 }
